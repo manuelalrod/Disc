f48bcd984809d451bb17678ce233998f519bfe96
Thana <aythanarios133@gmail.com>
2024-03-26 14:07:37 -0500

    diseño de la pag

--- disc1/disc1/settings.py
+++ disc1/disc1/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
-
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "main",
 ]
 
 MIDDLEWARE = [
@@ -120,7 +116,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- disc1/main/views.py
+++ disc1/main/views.py
@@ -1,13 +1,3 @@
 from django.shortcuts import render
 
-
-def home(request):
-    return render(request, 'home.html')
-
-
-def detalle(request):
-    return render(request, 'detalle.html')
-
-
-def funcion(request):
-    return render(request, 'funcion.html')
+# Create your views here.
