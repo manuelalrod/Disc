03c48bdb36a90af96dbee96c985842c0a3f58d1c
manuelalrod <manuelalmengor32@gmail.com>
2024-03-26 01:09:52 -0500

    puse mas html

--- disc1/main/urls.py
+++ disc1/main/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('detalle/', views.detalle, name="detalle"),
+    path('funcion/', views.funcion, name="funcion"),
 ]
--- disc1/main/views.py
+++ disc1/main/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def detalle(request):
+    return render(request, 'detalle.html')
+
+
+def funcion(request):
+    return render(request, 'funcion.html')
